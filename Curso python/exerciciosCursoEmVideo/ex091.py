380e365f4a9825a867baf77983c4b31c24e2c4bd
Igor <113212275+CEFET-Igor@users.noreply.github.com>
2023-11-08 22:11:57 -0300

    Update ex091.py

--- Curso python/exerciciosCursoEmVideo/ex091.py
+++ Curso python/exerciciosCursoEmVideo/ex091.py
@@ -1,13 +1,18 @@
 from random import randint
 from time import sleep
-dados = {'Jogador 1' : randint(1,6),
-         'Jogador 2' : randint(1,6),
-         'Jogador 3' : randint(1,6),
-         'Jogador 4' : randint(1,6)}
+dados = [{'nome': 'Jogador 1', 'valor': randint(1,6)},
+         {'nome': 'Jogador 2', 'valor': randint(1,6)},
+         {'nome': 'Jogador 3', 'valor': randint(1,6)},
+         {'nome': 'Jogador 4', 'valor': randint(1,6)}]
 classificação = []
 print('Valores sorteados: ')
-for key, val in dados.items():
-    print(f'\tO {key} tirou {val}')
-    sleep(0.5)
-    if len(classificação) == 0:
-        classificação.append(f'{key} com {val}')
+for dado in dados:
+    print(f'O {dado["nome"]} tirou {dado["valor"]}')
+    sleep(1)
+
+dados.sort(key=lambda x: x['valor'], reverse=True)
+
+print('Ranking dos jogadores: ')
+for pos in range(len(dados), 0, -1):
+    print(f'{pos}º lugar: {dados[pos - 1]["nome"]} com {dados[pos - 1]["valor"]}')
+    sleep(1)
