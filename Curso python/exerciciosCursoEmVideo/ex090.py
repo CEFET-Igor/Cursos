f36337386d602d5c94a88d6e361f6a9b724bb48a
Igor <113212275+CEFET-Igor@users.noreply.github.com>
2023-11-09 20:23:45 -0300

    Update ex090.py

--- Curso python/exerciciosCursoEmVideo/ex090.py
+++ Curso python/exerciciosCursoEmVideo/ex090.py
@@ -2,9 +2,11 @@ ficha = {}
 
 ficha['nome'] = str(input('Nome do aluno: '))
 ficha['media'] = float(input(f'Digite a media do aluno {ficha["nome"].capitalize()}: '))
-if 0 <= ficha['media'] < 6:
+if 0 <= ficha['media'] < 5:
     ficha['situação'] = 'Reprovado'
-elif 6 <= ficha['media'] <= 10:
+elif 5 <= ficha['media'] < 7:
+    ficha['situação'] = 'Recuperação'
+elif 7 <= ficha['media'] <= 10:
     ficha['situação'] = 'Aprovado'
 elif ficha['media'] > 10:
     ficha['situação'] = 'Nota maior que 10'
@@ -12,7 +14,8 @@ else:
     ficha['situação'] = 'Nota menor que 0'
     
 print(f"""
-Nome:     {ficha['nome'].capitalize()}
-Media:    {ficha['media']:.2f}
-Situação: {ficha['situação']}
+{'Nome:':<12}{ficha['nome'].capitalize()}
+{'Media:':<12}{ficha['media']:.2f}
+{'Situação:':<12}{ficha['situação']}
+{'-' * 30}
 """)
