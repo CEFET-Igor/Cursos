45ffbe46ad2f89ede287cda9bc97c634b83f533c
Igor <113212275+CEFET-Igor@users.noreply.github.com>
2023-11-10 20:46:04 -0300

    Update ex103.py

--- Curso python/exerciciosCursoEmVideo/ex103.py
+++ Curso python/exerciciosCursoEmVideo/ex103.py
@@ -1,13 +1,20 @@
 def ficha(nome='<desconhecido>', gols=0):
+    """
+    Função que exibe a ficha de um jogador.
+
+    :param nome: Nome do jogador (opcional)
+    :param gols: Quantidade de gols que o jogador fez (opcional)
+    :return: Escreve a ficha do jogador
+    """
     print(f'O jogador {nome} fez {gols} gol(s)')
 
 
 print('-' * 30)
 n = str(input('Nome do jogador: ')).strip().capitalize()
 g = input('Número de gols: ')
-if n == '' and g == '':
+if n == '' and (not g.isnumeric()):
     ficha()
-elif g == '':
+elif not g.isnumeric() :
     ficha(n)
 elif n == '':
     ficha(gols=g)
