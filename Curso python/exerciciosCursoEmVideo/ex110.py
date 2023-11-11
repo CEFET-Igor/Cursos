4ed40d8e217a3f3278882e7c26cd70b30bc2d287
Igor <113212275+CEFET-Igor@users.noreply.github.com>
2023-11-10 22:31:19 -0300

    Exercicios Aula 22

--- Curso python/exerciciosCursoEmVideo/ex110.py
+++ Curso python/exerciciosCursoEmVideo/ex110.py
@@ -1,1 +1,6 @@
-Hello World
+from uteis.moeda import moeda
+from os import system
+
+valor = float(input('Digite o preço: R$ '))
+system('clear')
+moeda.resumo(valor, 10, 10)
--- Curso python/exerciciosCursoEmVideo/ex112.py
+++ Curso python/exerciciosCursoEmVideo/ex112.py
@@ -1,1 +1,8 @@
-Hello World
+from uteis.numeros import dinheiro
+from uteis.moeda import moeda
+from os import system
+
+
+valor = dinheiro.leiaDinheiro('Digite o preço: R$ ')
+system('clear')
+moeda.resumo(valor)
