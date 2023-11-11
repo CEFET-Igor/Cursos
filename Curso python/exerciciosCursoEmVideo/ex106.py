023d8459f03938bf70a396dee64ddf73400393bc
Igor <113212275+CEFET-Igor@users.noreply.github.com>
2023-11-10 21:27:04 -0300

    Update ex106.py

--- Curso python/exerciciosCursoEmVideo/ex106.py
+++ Curso python/exerciciosCursoEmVideo/ex106.py
@@ -1,8 +1,20 @@
 from ex097 import escreveMsg
 from time import sleep
+cores = {
+        'vermelho': '\033[91m',
+        'verde': '\033[92m',
+        'amarelo': '\033[93m',
+        'azul': '\033[94m',
+        'roxo': '\033[95m',
+        'ciano': '\033[96m',
+        'branco': '\033[97m',
+        'fundo branco letra preta': '\033[107m',
+        'reset': '\033[0m'
+    }
 
 def ajuda(comando):
-    print(help(comando))
+    escreveMsg(f'{cores["fundo branco letra preta"]}Acessando o manual do comando {cores["reset"]}')
+    help(comando)
     sleep(1)
     return
 
